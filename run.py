35bad39610cc59606da4154d307ad530771b6768
jamesmukuya <james.mukuya@gmail.com>
2020-04-15 10:05:51 -0400

    new passesnger_wsgi, run.py edited

--- run.py
+++ run.py
@@ -1,10 +1,13 @@
-"""
-file to run the app locally
-"""
-from main import app
+import os
+import sys
 
-def main():
-	app.run(debug=True)
 
-if __name__ == "__main__":
-	main()
+sys.path.insert(0, os.path.dirname(__file__))
+
+
+def app(environ, start_response):
+    start_response('200 OK', [('Content-Type', 'text/plain')])
+    message = 'It works!\n'
+    version = 'Python v' + sys.version.split()[0] + '\n'
+    response = '\n'.join([message, version])
+    return [response.encode()]
