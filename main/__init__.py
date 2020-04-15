630dba0597238812bdee2e2e254929e207f8a9e6
jamesmukuya <james.mukuya@gmail.com>
2020-04-15 19:06:23 +0300

    are we a fit page

--- main/__init__.py
+++ main/__init__.py
@@ -10,5 +10,8 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = '7daf12b8731868c589b15b625907dd7b'
 
 from main.landingPage.route import landing_page
+from main.areWeAfit.route import areWeAfit_page
 
 app.register_blueprint(landing_page)
+app.register_blueprint(areWeAfit_page)
+
---
+++ main/areWeAfit/route.py
@@ -0,0 +1,11 @@
+"""
+are we a fit page
+"""
+from flask import Blueprint
+from flask import redirect, url_for, render_template
+areWeAfit_page = Blueprint('areWeAfit_page', __name__)
+
+@areWeAfit_page.route("/fit")
+def index():
+	context = {}
+	return render_template("areWeAfitHtml/are-we-a-fit.html", title="Are we a fit", **context)
