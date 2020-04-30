ee8bdc69665b28ea5e03c87df706d39b70514af7
jamesmukuya <james.mukuya@gmail.com>
2020-04-30 18:55:23 +0300

    email download design start

--- main/resources/route.py
+++ main/resources/route.py
@@ -2,6 +2,10 @@
 render and control resources
 the filename is in the db, the route is available in our downloads folder
 """
+import smtplib,os
+from main import app
+from email.message import EmailMessage
+
 from main.resources.form import ResourceRequestForm
 from flask import (Blueprint, render_template,flash, redirect, request, url_for)
 
@@ -16,9 +20,37 @@ def resources_page():
     context={'form':form,'filename':filename}
     return render_template('resources/resources.html', title='Resources', **context)
 
-
-@resources.route("/resources-link")
+@resources.route("/resources-request",methods=["POST"])
 def resources_get_link():
-    # use an expiring token 10mins
-    # use a uuid per download
-    pass
+    """
+    accepts post requests with the filename and email address to send to.
+    we then get the file and send as attachment to the email
+    """
+    return 'this is a file request'
+
+@resources.route("/mail-trial")
+def mail_send():
+    msg = EmailMessage()
+    msg['subject'] = 'Hello there'
+    msg['From'] = 'info@techpoint.systems'
+    msg['To'] = 'james.mukuya@gmail.com'
+    msg.set_content('this is an email from python')# this is the body
+    
+    #sending an attachement
+    file_name = 'link.txt'
+    file_path = app.config['MISC_UPLOAD_FOLDER']
+    full_path = os.path.join(file_path, file_name)
+    
+    with open(full_path, 'rb') as f:
+        file_data = f.read()
+
+    msg.add_attachment(file_data, maintype='application', subtype='octet-stream',
+        filename=file_name)
+    
+    #with smtplib.SMTP_SSL('smtp.gmail.com',465) as smtp:
+    with smtplib.SMTP_SSL('techpoint.systems',465) as smtp:
+        #smtp.login('james.mukuya@gmail.com', 'kq@12345')
+        smtp.login('info@techpoint.systems', 'K@zumi@1')
+        smtp.send_message(msg)
+
+    return 'this message was sent'
--- main/static/js/custom/resourceRequest.js
+++ main/static/js/custom/resourceRequest.js
@@ -7,10 +7,14 @@ const elements = {
     //resource: document.querySelectorAll('.resource'),
     resource_link: document.querySelectorAll('.resource-link'),
     resource_download: document.querySelectorAll('.resource-download'),
-    email_resource: document.getElementById('emailResource'),
+    email_div: document.getElementById('emailResource'),
+    email_address: document.getElementById('emailAddress'),
+    email_submit: document.querySelectorAll('.email-submit'),
 };
+    
 //convert from nodelist to array
 const resource_link_array = Array.from(elements.resource_link);
+const email_submit_array = Array.from(elements.email_submit);
 //let resource_array = Array.from(elements.resource);
 //const resource_download_array = Array.from(elements.resource_download);
 //const resource_name_array = Array.from(elements.resource_name);
@@ -19,14 +23,51 @@ const resource_link_array = Array.from(elements.resource_link);
 resource_link_array.forEach(el_link => {
     el_link.addEventListener('click', getLink);
 });
+
+email_submit_array.forEach(el_btn => {
+    el_btn.addEventListener('click', postMail);
+});
+
 function getLink(el) {
     //display the email request link
-    elements.email_resource.classList.toggle("hidden");
-    //get resource id from parent div
-    //parentId = el.target.parentNode.parentNode.id;
-    //post request to required path
-}
+    elements.email_div.classList.toggle("hidden");
+    //console.log(filename);
+    
+};
 
+// when the user interacts with the email box
+function postMail(e) {
+    // declare a variable object to hold the post data
+    let file_content_request;
+
+    // get email address from input
+    let email_address = elements.email_address.value;
+
+    // create a post object
+    file_content_request = {
+        "filename": e.target.id,
+        "email": email_address,
+    };
+    // console.log(file_content_request);
+
+    // hide the email address box
+    elements.email_div.classList.add("hidden");
+};
+
+/*
+let postMail = function() {
+    //post request to required path
+    fetch(`${window.location.origin}/resources-request`, {
+        method: "POST",
+        credentials: "include",
+        body: JSON.stringify(),
+        cache: "no-cache",
+        headers: new Headers({
+            "content-type": "application/json"
+        })
+    })
+}
+*/
 /*
 resource_download_array.forEach(el_dwn => {
     el_dwn.addEventListener('click', download);
